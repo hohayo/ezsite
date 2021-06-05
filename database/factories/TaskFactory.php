6eb0c5fca489e41a47ba2c2e4956079afb98084a
Kyle, Chuang <hohayo@gmail.com>
2021-06-05 15:44:03 +0800

    [feat] faker factory

--- database/factories/TaskFactory.php
+++ database/factories/TaskFactory.php
@@ -21,13 +21,11 @@ class TaskFactory extends Factory
      */
     public function definition()
     {
-        $rdm = rand(0, 10000000);
-
         return [
-            'title' => '打掃家裡',
-            'salary' => $rdm,
-            'desc' => "幫忙掃個地 $rdm 分鐘",
-            'enabled' => true,
+            'title' => $this->faker->realText(15),
+            'salary' => rand(0, 10000000),
+            'desc' => $this->faker->realText,
+            'enabled' => $this->faker->randomElement(array(true, false)),
         ];
     }
 }
