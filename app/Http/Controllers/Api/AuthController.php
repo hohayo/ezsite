68920bdd88410b6594188d818afb470e0a17607e
Kyle, Chuang <hohayo@gmail.com>
2021-06-06 15:44:47 +0800

    [feat] add AuthController

--- app/Http/Controllers/Api/AuthController.php
+++ app/Http/Controllers/Api/AuthController.php
@@ -7,5 +7,28 @@ use Illuminate\Http\Request;
 
 class AuthController extends Controller
 {
-    //
+    public function __construct()
+    {
+        $this->middleware('auth:api')->except('login');
+    }
+
+    public function login()
+    {
+        $credentials = request(['email', 'password']);
+        if (! $token = auth()->attempt($credentials)) {
+            return response()->json(['status' => 0, 'message' => '無效的驗證資料'], 401);
+        }
+        return response()->json(['status' => 1, 'token' => $token]);
+    }
+
+    public function me()
+    {
+        return response()->json(auth()->user());
+    }
+
+    public function logout()
+    {
+        auth()->logout();
+        return response()->json(['status' => 1]);
+    }
 }
