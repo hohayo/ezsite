5bf027cc45161ba1b11905a6fb58013bf306c357
Kyle, Chuang <hohayo@gmail.com>
2021-06-07 00:46:52 +0800

    [faet] add Article api

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,8 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
+use App\Models\Article;
+
 /*
 |--------------------------------------------------------------------------
 | API Routes
@@ -12,10 +14,31 @@ use Illuminate\Support\Facades\Route;
 | routes are loaded by the RouteServiceProvider within a group which
 | is assigned the "api" middleware group. Enjoy building your API!
 |
- */
+*/
 
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
+});
+
+Route::post('/hw/times',  'App\Http\Controllers\Api\HwController@times');
+
+Route::get('/name/{name}', 'App\Http\Controllers\Api\HelloController@hello');
+
+Route::apiResource('tasks', 'App\Http\Controllers\Api\TaskController');
+Route::get('tasks/title/query', 'App\Http\Controllers\Api\TaskController@query');
+
+Route::middleware('auth:api')->group(function() {
+
+});
+
+Route::apiResource('categories', 'App\Http\Controllers\Api\CategoryController');
+Route::apiResource('posts', 'App\Http\Controllers\Api\PostController');
+Route::apiResource('tags', 'App\Http\Controllers\Api\TagController');
+
+Route::group(['prefix' => 'auth', 'namespace' => 'App\Http\Controllers\Api'], function () {
+    Route::get('/', 'AuthController@me')->name('me');
+    Route::post('login', 'AuthController@login')->name('login');
+    Route::post('logout', 'AuthController@logout')->name('logout');
 });
 
 Route::group(['namespace' => '\Javck\Ezlaravel\Http\Controllers'], function () {
@@ -25,4 +48,14 @@ Route::group(['namespace' => '\Javck\Ezlaravel\Http\Controllers'], function () {
     Route::post('areas/queryByCounty', 'ApiController@queryAreas');
     Route::post('elements/queryPositions', 'ApiController@queryPositions');
     Route::post('elements/queryElementModes', 'ApiController@queryElementModes');
+});
+
+Route::get('/posts',function() {
+
+    $articles = Article::get();
+
+    return response()->json([
+        'status' => 1,
+        'data' => $articles
+    ])->setEncodingOptions(JSON_UNESCAPED_UNICODE);
 });
